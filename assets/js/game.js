0e210013060e8d3682ea59786a846e23999a5dfc
toryua <87789457+toryua@users.noreply.github.com>
2021-08-10 13:37:13 -0700

    robot health check - if / else statements

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,7 +9,7 @@ var enemyName = "Roborto";
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function () {
     // Alert plauyers that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -21,7 +21,22 @@ var fight = function() {
     /* Log a resulting message to the console so we know it worked */
     console.log(
         playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
-    
+
+    // check player's health
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+        }
+    else {
+        window.alert(playerName+ " still has " + playerHealth + " health left.");
+    }
+    // check enemy's health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
     /* Subtract the value of 'enemyAttack' from the value 
     of 'playerHealth' and use that result to update the value 
     in the 'playerHealth' variable. */
